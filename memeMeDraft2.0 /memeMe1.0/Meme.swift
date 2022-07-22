30dbf206c8a9574195d858ec27e2e9170ce50184
Oladele Abimbola <96730966+Oladele-A@users.noreply.github.com>
2022-07-22 16:34:08 +0100

    Add files via upload

---
+++ memeMeDraft2.0 /memeMe1.0/Meme.swift
@@ -0,0 +1,16 @@
+//
+// Meme.swift
+//  memeMe1.0
+//
+//  Created by macOS on 4/12/22.
+//
+
+import Foundation
+import UIKit
+
+struct Meme{
+    var topText:String
+    var bottomText:String
+    var originalImage:UIImage
+    var memedImage:UIImage
+}
---
+++ memeMeDraft2.0 /memeMe1.0/Table and Collection Controller/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  memeMe1.0
+//
+//  Created by Oladele Abimbola on 4/25/22.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var collectionCellImage: UIImageView!
+}
